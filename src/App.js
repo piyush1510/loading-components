255460c56caea701a4d3549e791177bd3b9081bf
Piyush Verma <piyus15101999@gmail.com>
2021-05-22 12:28:19 +0530

    basic css loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Loader1 from './loaders/Loader1';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="stage"><Loader1 /></div>
     </div>
   );
 }
---
+++ src/loaders/Loader1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Loader.css'
+function Loader1() {
+    return (
+        <div className="loader-1">
+            
+        </div>
+    )
+}
+
+export default Loader1
